64c5cce8e104a120e811ddec1d9498341b4b5e36
Jalussa dos Santos <jalussa.santos@gmail.com>
2021-04-13 11:43:03 -0300

    interagindo com selectbox

--- cypress/integration/exampleBasics/tickets.spec.js
+++ cypress/integration/exampleBasics/tickets.spec.js
@@ -1,7 +1,8 @@
 describe("Tickets",() =>{
     beforeEach(() => cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html"));
 
-    it.only("Preencher todos os campos de texto",()=>{
+    
+    it("Preencher todos os campos de texto",()=>{
 
         const firstName = "Teste";
         const lastName = "Cypress";
@@ -11,6 +12,11 @@ describe("Tickets",() =>{
         cy.get("#email").type("testecypress@testes.com.br");
         cy.get("#requests").type("testes123");
         cy.get("#signature").type(`${firstName} ${lastName}`);
+    });
+
+    it.only("Selecionar 2 tickets",() => {
+        cy.get("#ticket-quantity").select("2");
+
     });
 
     it("has 'TICKETBOX' headers's heading",() =>{});
