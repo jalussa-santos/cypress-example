677e2211a996ea14a276068008314305d251b274
Jalussa dos Santos <jalussa.santos@gmail.com>
2021-04-19 21:14:04 -0300

    novo script para alterar a baseUrl Padrão

--- cypress/integration/exemploBasico/tickets.spec.js
+++ cypress/integration/exemploBasico/tickets.spec.js
@@ -1,5 +1,5 @@
 describe("Tickets", () =>{
-    beforeEach(() => cy.visit());
+    beforeEach(() => cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html"));
 
     
     it("Preencher todos os campos de texto",()=>{
