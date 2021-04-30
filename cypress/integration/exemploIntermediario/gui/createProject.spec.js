753a7b7f742d6869ad66305ff1989319eb8ca905
Jalussa dos Santos <jalussa.santos@gmail.com>
2021-04-29 22:50:23 -0300

    incluindo teste criarProjeto

---
+++ cypress/integration/exemploIntermediario/gui/createProject.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="Cypress"/>
+
+const faker = require('faker');
+
+describe('Criar Projeto', () => {
+   before(() => cy.login());
+
+   it('sucesso', ()=>{
+    const project = {
+        name: `project=${faker.random.uuid}`,
+        description: faker.random.words(5)
+    }
+
+    cy.gui_createProject(project);
+    cy.url().should('be.equal',`${Cypress.config('baseUrl')}${Cypress.env('user_name')}/${project.name}`);
+    cy.contains(project.name).should('be.visible');
+    cy.contains(project.description).should('be.visible');
+    });
+});
