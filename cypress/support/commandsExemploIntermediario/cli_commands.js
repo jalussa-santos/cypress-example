0fe915bf7a722a0f1b84bfde432dfa64445940cb
Jalussa dos Santos <jalussa.santos@gmail.com>
2021-05-31 22:25:35 -0300

    removendo comentário

--- cypress/support/commandsExemploIntermediario/cli_commands.js
+++ cypress/support/commandsExemploIntermediario/cli_commands.js
@@ -3,6 +3,5 @@
 Cypress.Commands.add('cloneViaHttp', project => {
     const domain = Cypress.config('baseUrl').replace('http://', '').replace('/', '')
   
-    /*cy.exec(`cd temp/ && git clone git@${domain}:${Cypress.env('user_name')}/${project.name}.git`)*/
     cy.exec(`cd temp/ && git clone http://${domain}/${Cypress.env('user_name')}/${project.name}.git`)
   })
