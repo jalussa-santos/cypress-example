4da60cf95304f2cdb22a85c85378b8d9c42a3a94
Jalussa dos Santos <jalussa.santos@gmail.com>
2021-06-05 23:17:57 -0300

    config inicial

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,3 +2,4 @@ import './commandsExemploBasico/commands'
 import './commandsExemploIntermediario/gui_commands'
 import './commandsExemploIntermediario/api_commands'
 import './commandsExemploIntermediario/cli_commands'
+import '@percy/cypress';
