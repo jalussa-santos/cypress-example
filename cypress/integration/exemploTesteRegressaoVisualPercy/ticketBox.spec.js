cde698c4aa0335971cfd0e57b11d45b833e1cfef
Jalussa dos Santos <jalussa_santos@sicredi.com.br>
2021-07-07 21:53:42 -0300

    ajustes projeto

--- cypress/integration/exemploTesteRegressaoVisualPercy/ticketBox.spec.js
+++ cypress/integration/exemploTesteRegressaoVisualPercy/ticketBox.spec.js
@@ -1,6 +1,4 @@
 describe('Ticketbox', () => {
-
-   //teste
     beforeEach(() => cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html"));
     it('Checar o estado inicial da aplicação', () => {
        cy.percySnapshot();      
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,5 +2,5 @@ import './commandsExemploBasico/commands'
 import './commandsExemploIntermediario/gui_commands'
 import './commandsExemploIntermediario/api_commands'
 import './commandsExemploIntermediario/cli_commands'
-import '@percy/cypress'
+import '@percy/cypress';
 import 'cypress-plugin-snapshots/commands'
