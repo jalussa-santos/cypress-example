cd3469b753a37974b0a692ddc48dd99e375ac3f5
Jalussa dos Santos <jalussa_santos@sicredi.com.br>
2021-07-07 21:49:24 -0300

    Atualização cypress

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,7 +2,5 @@ import './commandsExemploBasico/commands'
 import './commandsExemploIntermediario/gui_commands'
 import './commandsExemploIntermediario/api_commands'
 import './commandsExemploIntermediario/cli_commands'
-import '@percy/cypress';
-
-//Importa o plugin para os snapshots
-import 'cypress-plugin-snapshots/commands';
+import '@percy/cypress'
+import 'cypress-plugin-snapshots/commands'
