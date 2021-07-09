d3a4b8f4517f0d116fc685af15ff9d32e5e29068
Jalussa dos Santos <jalussa_santos@sicredi.com.br>
2021-07-09 20:24:48 -0300

    add testes para refatorar

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,5 +2,6 @@ import './commandsExemploBasico/commands'
 import './commandsExemploIntermediario/gui_commands'
 import './commandsExemploIntermediario/api_commands'
 import './commandsExemploIntermediario/cli_commands'
+import './commandsExemploAvancado/commands'
 import '@percy/cypress';
 import 'cypress-plugin-snapshots/commands'
